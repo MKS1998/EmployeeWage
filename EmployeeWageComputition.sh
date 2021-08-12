653d4de9f52046ba4de360986b6078705ca41357
MKS1998 <nksayyad2900@outlook.com>
2021-08-12 20:57:06 +0530

    Added EmployeeWage Attendnce

--- EmployeeWageComputition.sh
+++ EmployeeWageComputition.sh
@@ -1,3 +1,15 @@
 #!  /bin/bash   -x
 
 echo "Welcome Git Hub"
+Present=1
+randomCheck=$((RANDOM%2))
+empRatePerHr=20
+workHr=8
+if [ $randomCheck -eq $Present ]
+then
+	salary=$(( $empRatePerHr * $workHr ))
+	echo "Employee is Present"
+else
+	echo "Employee is NOT Present"
+fi 
+echo "salary = $salary"
